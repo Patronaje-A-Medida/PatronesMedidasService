f892566c75e18dafc39dce0ee4675516c09ef682
Geraldine Curipaco <geraldinenisbeth@gmail.com>
2022-06-13 22:53:28 -0500

    agregado pyrebase

--- src/api/controllers/body_measurements_controller.py
+++ src/api/controllers/body_measurements_controller.py
@@ -2,7 +2,21 @@ from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 from business.services.body_measurements_service import BodyMeasurementsService
 from business.handlers.service_exception import ServiceException
+import pyrebase
 
+config = {
+    "apiKey": "AIzaSyD6rwCtpBQrIVLah_BFVjGjt6W4XUwYfPw",
+    "authDomain": "pry2021251-pam.firebaseapp.com",
+    "databaseURL": "",
+    "projectId": "pry2021251-pam",
+    "storageBucket": "pry2021251-pam.appspot.com",
+    "messagingSenderId": "101974237012",
+    "appId": "1:101974237012:web:38e2c349736ecf2fbe32c1"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+auth = firebase.auth()
 router = APIRouter()
 
 
@@ -20,8 +34,12 @@ async def take_measurements(
         height: float = Form(...),
         service: BodyMeasurementsService = Depends(BodyMeasurementsService)) -> dict:
     try:
-        result = await service.take_measurements(file_frontal, file_lateral, height, client_id)
-        return JSONResponse(status_code=200, content=result.to_map())
+        #result = await service.take_measurements(file_frontal, file_lateral, height, client_id)
+        #return JSONResponse(status_code=200, content=result.to_map())
+        user = auth.sign_in_with_email_and_password('u201716506@upc.edu.pe', 'cocobicho1413')
+        storage.child('data/'+file_frontal.filename).put(file_frontal.file, user['idToken'])
+        storage.child('data/'+file_lateral.filename).put(file_lateral.file, user['idToken'])
+        return JSONResponse(status_code=200, content={'ok':'ok'})
     except ServiceException as ex:
         return JSONResponse(status_code=500, content={"statusCode": 500, "errorCode": ex.error_code, "message": ex.error_message})
 
